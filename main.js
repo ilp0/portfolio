3ad8da22b6829574b1342344e5485aa34f837072
Ilpo Loikkanen <Ilpo Loikkanen>
2017-11-27 18:14:40 +0200

    added mobile menu

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-function(){
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop(); // how many pixels you've scrolled
-        var os = $('#div1').offset().top; // pixels to the top of div1
-        var ht = $('#div1').height(); // height of div1 in pixels
-        // if you've scrolled further than the top of div1 plus it's height
-        // change the color. either by adding a class or setting a css property
-        if(scroll > os + ht){
-            $('#div2').addClass('blue');
-        }
-    });
-});
+function myFunction() {
+    var x = document.getElementById("mobileNavBar");
+    if (x.className === "mobnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "mobnav";
+    }
+}
