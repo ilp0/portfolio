9a0305ae87ae8b8b113819f2cefbabc8d721f290
Ilpo Loikkanen <Ilpo Loikkanen>
2017-11-28 15:36:49 +0200

    added js minigame, fixed pf cards

--- main.js
+++ main.js
@@ -6,3 +6,25 @@ function myFunction() {
         x.className = "mobnav";
     }
 }
+
+var count = 0;
+function klikkeri(){
+   
+    var text = document.getElementById("profilesubtitle");
+    count += 1;
+    text.innerHTML = count;
+    if(count >= 50 && count < 55){
+        text.innerHTML = "lopeta jo"
+    } 
+    if(count > 99 && count < 105){
+        text.innerHTML = "kaikki ok?"
+    }
+    if(count > 249 && count < 255){
+        text.innerHTML = "tästä lähti ilmoitus ilpolle ip-osoitteesi kera että täällä joku klikkailee hulluna profiilikuvaa";        
+    }
+    if (count > 999){
+        text.innerHTML = "voitit pelin, luuseri."
+    }
+}
+
+
