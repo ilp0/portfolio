66e6fd6fa53a72ead7bf1e4ad3d7f705a27e34d8
Ilpo Loikkanen <ilpo.loikkanen@hotmail.com>
2018-05-22 11:06:48 +0300

    changed img width css

--- p/pf_card.php
+++ p/pf_card.php
@@ -3,7 +3,7 @@
 function createPfCard($imagepath, $imagelink, $title, $text){
     echo('<div class="kortti">
         <div class="web_korttiImg" style="min-width: 200px; max-width: 300px;">
-            <a href="' . $imagelink . '"><img src="' . $imagepath . '" alt="' . $title . '" style="width:100%"></a>
+            <a href="' . $imagelink . '"><img src="' . $imagepath . '" alt="' . $title . '"></a>
         </div>
         <div class="korttiContainer">
             <h3>' . $title . '</h3>
